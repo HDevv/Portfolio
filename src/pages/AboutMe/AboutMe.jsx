368eda2f1d183e415c313f1cbb78b99225a2eb3b
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2023-06-08 16:59:24 +0200

    intégration du txt dans la page welcome

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./../AboutMe/AboutMe.css";
 
 const AboutMe = () => {
-  return <div></div>;
+  return (
+    <div className="about_me">
+      <div className="about_txt">
+        <p className="about_title">WELCOME</p>
+        <ul className="sub_txt">
+          <li>
+            Full-stack web developer passionate about PROBLEM SOLVING and
+            CREATING
+          </li>
+          <li>
+            I regularly carry out technology watches to keep abreast of the
+            latest updates
+          </li>
+          <li>
+            I come out of a professional training in which I had to present big
+            projects in front of a jury, carried out with a mentor and also
+            independently finally having my diploma,
+          </li>
+          <li>
+            I work from time to time for individuals and continue to train on my
+            personal side projects
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
